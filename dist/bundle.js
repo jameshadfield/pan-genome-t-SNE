a7331a6667eda902ccda64560e6e1cbb7e2c1f5c
jameshadfield <jh22@sanger.ac.uk>
2016-06-11 17:52:44 +0100

    better help info

--- dist/bundle.js
+++ dist/bundle.js
@@ -12274,14 +12274,20 @@
 	  var metadataFileName = window.metadata ? window.metadata.fileName : 'not loaded';
 	  var metadataColumnName = window.metadata ? window.metadata.headerNames[window.metadata.colToUse] : 'N/A';
 
+	  var auth = '<p>pan genome t-SNE // james hadfield // version 0.1</p>';
 	  var el = document.getElementById('info');
-	  el.innerHTML = '<p>pan genome t-SNE // james hadfield // version 0.1</p>' + '<p>ROARY data: ' + roaryName + '</br>' + 'metadata: ' + metadataFileName + '</br>' + 'metadata column: ' + metadataColumnName + '</br>' + 'num t-SNE iterations: ' + numTSNEIters + '</p>';
+
+	  if (roaryName === 'not loaded' && metadataFileName === 'not loaded') {
+	    el.innerHTML = auth + '<p><strong>how to use:</strong></br>' + 'Drag on some ROARY results (gene_presence_absence.csv file) ' + 'and (optionally) a CSV file linking taxa with metadata.</br>' + 'The metadata is used to colour the plots on the scatterplot</br>' + 'Pressing 1-9 cycles through metadata columns</p>' + '<p>Currently ' + numTSNEIters + ' iterations will be run, which may be slow (check the javascript console to see progress)</p>';
+	  } else {
+	    el.innerHTML = auth + '<p>ROARY data: ' + roaryName + '</br>' + 'metadata: ' + metadataFileName + '</br>' + 'metadata column: ' + metadataColumnName + '</br>' + 'num t-SNE iterations: ' + numTSNEIters + '</p>';
+	  }
 	}
 
 	function keyIncoming(event) {
-	  // console.log('keyIncoming triggered');
+	  var key = event.keyCode || event.charCode;
+	  // console.log('key press. Code =', key);
 	  if (window.metadata) {
-	    var key = event.keyCode || event.charCode;
 	    if (key >= 49 && key <= 57) {
 	      window.metadata.colToUse = key - 49;
 	      (0, _graphing.updateScatter)(window.taxaNames);
--- js/index.js
+++ js/index.js
@@ -29,18 +29,30 @@ function updateInfo() {
   const metadataFileName = window.metadata ? window.metadata.fileName : 'not loaded';
   const metadataColumnName = window.metadata ? window.metadata.headerNames[window.metadata.colToUse] : 'N/A';
 
+  const auth = '<p>pan genome t-SNE // james hadfield // version 0.1</p>';
   const el = document.getElementById('info');
-  el.innerHTML = '<p>pan genome t-SNE // james hadfield // version 0.1</p>'
-    + '<p>ROARY data: ' + roaryName + '</br>'
-    + 'metadata: ' + metadataFileName + '</br>'
-    + 'metadata column: ' + metadataColumnName + '</br>'
-    + 'num t-SNE iterations: ' + numTSNEIters + '</p>';
+
+  if (roaryName === 'not loaded' && metadataFileName === 'not loaded') {
+    el.innerHTML = auth
+      + '<p><strong>how to use:</strong></br>'
+      + 'Drag on some ROARY results (gene_presence_absence.csv file) '
+      + 'and (optionally) a CSV file linking taxa with metadata.</br>'
+      + 'The metadata is used to colour the plots on the scatterplot</br>'
+      + 'Pressing 1-9 cycles through metadata columns</p>'
+      + '<p>Currently ' + numTSNEIters + ' iterations will be run, which may be slow (check the javascript console to see progress)</p>';
+  } else {
+    el.innerHTML = auth
+      + '<p>ROARY data: ' + roaryName + '</br>'
+      + 'metadata: ' + metadataFileName + '</br>'
+      + 'metadata column: ' + metadataColumnName + '</br>'
+      + 'num t-SNE iterations: ' + numTSNEIters + '</p>';
+  }
 }
 
 function keyIncoming(event) {
-  // console.log('keyIncoming triggered');
+  const key = event.keyCode || event.charCode;
+  // console.log('key press. Code =', key);
   if (window.metadata) {
-    const key = event.keyCode || event.charCode;
     if (key >= 49 && key <= 57) {
       window.metadata.colToUse = key - 49;
       updateScatter(window.taxaNames);
